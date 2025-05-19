a3cb61701bf8c7c9126a9017796fa9c8b0755a58
sarvar2003 <saravarjuraev@gmail.com>
2025-05-19 23:54:10 +0200

    Updated settings.py

--- eventure/settings.py
+++ eventure/settings.py
@@ -148,10 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
