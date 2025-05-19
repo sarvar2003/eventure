ffcd71ce55c695898154a84035c5abe972276ae6
sarvar2003 <saravarjuraev@gmail.com>
2025-05-19 23:47:53 +0200

    Updated settings.py

--- eventure/settings.py
+++ eventure/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.get_env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.get_env("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
