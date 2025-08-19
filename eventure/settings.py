b7dd0f1d757f874b07945177c52236121589dbd4
sarvar2003 <saravarjuraev@gmail.com>
2025-08-19 19:13:20 +0200

    Fixed allowed hosts setting

--- eventure/settings.py
+++ eventure/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["eventure.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["eventure-luqn.onrender.com", "localhost", "127.0.0.1"]
 
 
 
