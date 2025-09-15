c4306a6c86d5bef0e05b3022ccbcafd0943c111a
sarvar2003 <saravarjuraev@gmail.com>
2025-09-15 18:47:32 +0200

    Trying new things

--- eventure/settings.py
+++ eventure/settings.py
@@ -171,10 +171,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = "live.smtp.mailtrap.io"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+SERVER_EMAIL = os.getenv("SERVER_EMAIL")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
