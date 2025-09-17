bc947d41a76224b08a7b04d6c29ba84c8e9d2a92
sarvar2003 <saravarjuraev@gmail.com>
2025-09-17 16:55:04 +0200

    Debug=true

--- eventure/settings.py
+++ eventure/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["eventure-luqn.onrender.com", "localhost", "0.0.0.0", "api.eventures.world"]
 # ALLOWED_HOSTS = ["*"]
