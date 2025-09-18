1d817c554b176729a1acae57d6b609f5699eb77d
sarvar2003 <saravarjuraev@gmail.com>
2025-09-18 20:08:00 +0200

    Fixed bugs

--- eventure/settings.py
+++ eventure/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = "eventure.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/eventue-db',
+        default='postgresql://postgres:postgres@localhost:5432/eventue',
         conn_max_age=600
     )
 }
