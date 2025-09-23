0ac684177266858f4d676239d1ee0f98a1235e0f
sarvar2003 <saravarjuraev@gmail.com>
2025-09-23 18:03:40 +0200

    ?!

--- eventure/settings.py
+++ eventure/settings.py
@@ -112,10 +112,11 @@ WSGI_APPLICATION = "eventure.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/eventue',
+        conn_max_age=600
+    )
 }
 
 
