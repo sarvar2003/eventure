19b06f6cbfb49c0dbe2dbfe076a45a0f5ac2c49c
sarvar2003 <saravarjuraev@gmail.com>
2025-09-25 17:35:51 +0200

    Altered TopicsViewSet

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+topics_router = DefaultRouter()
+topics_router.register(r"topics", views.TopicsViewSet, basename="topics")
+
 
 urlpatterns = [
     path("", views.AllEventsAPIView.as_view(), name="all-events"),
@@ -8,5 +13,6 @@ urlpatterns = [
     path("event/<int:id>/", views.RetrieveEventAPIView.as_view(), name="event-info"),
     path("event/<int:id>/update/",views.UpdateEventAPIView.as_view(),name="update-event"),
     path("user-events/<str:email>/",views.ListUserEventsAPIView.as_view(), name="user-events"),
-    path("topics/", views.TopicsViewSet.as_view({"get": "list"}), name="topics"),
 ]
+
+urlpatterns += topics_router.urls
