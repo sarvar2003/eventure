fda9b860ef7cc5c54dedd2e7b987e1551555ae2f
sarvar2003 <saravarjuraev@gmail.com>
2025-10-02 19:37:05 +0200

    Fixed Media url

--- eventure/settings.py
+++ eventure/settings.py
@@ -176,7 +176,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
 
 # Email
 
